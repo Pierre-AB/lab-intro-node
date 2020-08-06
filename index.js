ee5a95c360e0d5f3551f9cd96d0a46df32e289f7
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-08-06 21:33:37 +0200

    iteration 3 done

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = this.items.length;
+    this.length = 0;
   }
   add(item) {
     this.items.push(item);
-    this.items.sort((a,b) => {
-      return a-b;
-    })
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length++;
   }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
   max() {}
 
